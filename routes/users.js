4f4478f2e3910edaaf04304f7ffd74d988edf9e0
Marcin Książek <marcin.ksiazek@estimote.com>
2018-01-31 13:48:58 +0100

    scaffold current user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.render('users_current', { title: 'My info' });
 });
 
 module.exports = router;
