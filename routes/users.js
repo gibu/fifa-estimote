a28645f502dc340ddbd81033fa845d74d4a978cf
Marcin Książek <marcin.ksiazek@estimote.com>
2018-02-01 11:02:11 +0100

    add winner_id

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,17 @@ const router = express.Router();
 const Db = require('../services/db');
 
 const formatMatches = (player) => {
+  return player.matches.map((match) => {
+    let winner_id = null;
+    if (match.played) {
+      if (match.player_1_points === 3) {
+        winner_id = match.player_1_id;
+      } else if (match.player_2_points) {
+        winner_id = match.player_2_id;
+      }
+    }
+    return Object.assign({}, match, {winner_id})
+  });
 };
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,7 +26,7 @@ router.get('/:nick', async function(req, res, next) {
   if (!player) {
     return res.status(404);
   }
-  // console.log('Player---', player);
+  player.matches = formatMatches(player);
   res.render('users_current', { title: req.params.nick, player: player });
 });
 
